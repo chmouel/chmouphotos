e7e98f683b3f10a8eb139c885d957ac8e68c3767
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-13 11:24:36 +0100

    set -xe
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- hack/ccredeploy.sh
+++ hack/ccredeploy.sh
@@ -8,5 +8,5 @@ targetDir="~/GIT/chmouphoto"
 env GOOS=linux GOARCH=arm GOARM=5 go build -o /tmp/rpi-chmouphoto
 ssh ${rpi} "cd ${targetDir};git pull "
 scp /tmp/rpi-chmouphoto ${rpi}:/tmp/chmouphoto 
-ssh ${rpi} "sudo systemctl stop chmouphoto && sudo mv /tmp/chmouphoto /usr/local/bin/chmouphoto && sudo systemctl start chmouphoto && journalctl -u chmouphoto.service --lines=10 --no-pager"
+ssh ${rpi} "set -xe;sudo systemctl stop chmouphoto && sudo mv /tmp/chmouphoto /usr/local/bin/chmouphoto && sudo systemctl start chmouphoto && journalctl -u chmouphoto.service --lines=10 --no-pager"
 
