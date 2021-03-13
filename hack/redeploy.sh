d33bac1c095ee0395d24ae2a713a7526abb6a220
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-13 11:06:16 +0100

    Fix script
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- hack/redeploy.sh
+++ hack/redeploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -x
 set -e
 
@@ -7,3 +7,4 @@ go build -o /tmp/chmouphoto main.go
 sudo systemctl stop chmouphoto 
 sudo mv /tmp/chmouphoto /usr/local/bin/chmouphoto 
 sudo systemctl start chmouphoto
+journalctl -u chmouphoto.service --lines=10 --no-pager
