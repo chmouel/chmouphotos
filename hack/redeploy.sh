d8e5dc7106afadf87866492448bb6766a87df778
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-19 22:32:51 +0100

    Compile with updated golang
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- hack/redeploy.sh
+++ hack/redeploy.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 git pull 
-[[ -x /tmp/chmouphoto ]] || go build -o /tmp/chmouphoto main.go 
+[[ -x /tmp/chmouphoto ]] || /usr/local/go/bin/go build -o /tmp/chmouphoto main.go 
 sudo systemctl stop chmouphotos
 sudo mv /tmp/chmouphoto /usr/local/bin/chmouphoto 
 sudo systemctl start chmouphotos
