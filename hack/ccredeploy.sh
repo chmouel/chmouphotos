9a83219e8c93862c9ffebb79e8c09d7dc00dddec
Chmouel Boudjnah <chmouel@chmouel.com>
2021-04-02 11:27:39 +0200

    Don't cross compile this doesn't work with sql/mysql
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- hack/ccredeploy.sh
+++ hack/ccredeploy.sh
@@ -4,8 +4,5 @@ set -e
 
 rpi=pi.lan
 targetDir="~/GIT/chmouphoto"
-
-env GOOS=linux GOARCH=arm GOARM=5 go build -o /tmp/rpi-chmouphoto
-scp /tmp/rpi-chmouphoto ${rpi}:/tmp/chmouphoto 
 ssh ${rpi} "cd ${targetDir};./hack/redeploy.sh"
 
