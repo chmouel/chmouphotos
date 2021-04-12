3313b66c894166480eb11bdf74dc137ad24cabf8
Chmouel Boudjnah <chmouel@chmouel.com>
2021-04-12 11:09:33 +0200

    Add make setup-dev which does everything for local dev
    sync from server database and photos

--- hack/setup-local-dev.sh
+++ hack/setup-local-dev.sh
@@ -10,9 +10,15 @@ CONTAINER_PORT=3306
 MYSQL_ROOT_PASSWORD=chmouel
 MYSQL_DATABASE=chmouphotos
 
+REMOTE_SERVER=pi
+REMOTE_PHOTOS=/home/www/photos/content/images
+LOCAL_PHOTOS=$HOME/Pictures/photos/content/
+
+rsync -avuz --delete ${REMOTE_SERVER}:${REMOTE_PHOTOS} ${LOCAL_PHOTOS}
+
 sudo docker stop ${CONTAINER_NAME} || true && sudo docker rm ${CONTAINER_NAME}
 sudo docker run -p ${CONTAINER_HOST}:${CONTAINER_PORT}:${CONTAINER_PORT} \
      --name ${CONTAINER_NAME} -d -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} ${CONTAINER_IMAGE}
 sleep 10
 mysqladmin -h${CONTAINER_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} create ${MYSQL_DATABASE}
-mysqldump --defaults-group-suffix=pi --add-drop-database ${MYSQL_DATABASE}|mysql ${MYSQL_DATABASE}
+mysqldump --defaults-group-suffix=${REMOTE_SERVER} --add-drop-database ${MYSQL_DATABASE}|mysql ${MYSQL_DATABASE}
